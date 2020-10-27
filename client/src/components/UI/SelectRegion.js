229bb43a5cb49e4f42d241a2c0043f1ce1b0e15b
shalynishka225 <shalynishka225@gmail.com>
2020-10-27 14:16:45 +0200

    fetch data from region API

--- client/src/components/UI/SelectRegion.js
+++ client/src/components/UI/SelectRegion.js
@@ -1,21 +1,14 @@
-import { Select } from 'antd';
-import React from 'react';
+import { Select } from "antd";
+import React from "react";
 
 const { Option } = Select;
 
 export const SelectRegion = (props) => {
   return (
-    <Select
-      defaultValue="Виберіть область"
-      style={{ width: '100%' }}
-      onChange={() => {}}
-    >
-      <Option value="Одеська область">Одеська область</Option>
-      <Option value="Дніпропетровська область">Дніпропетровська область</Option>
-      <Option value="Чернігівська область">Чернігівська область</Option>
-      <Option value="Харківська область">Харківська область</Option>
-      <Option value="Житомирська область">Житомирська область</Option>
-      <Option value="Полтавська область">Полтавська область</Option>
+    <Select defaultValue="Виберіть область" style={{ width: "100%" }}>
+      {props.data.map((item, index) => {
+        return <Option value={item.name + ""} key={index} />;
+      })}
     </Select>
   );
 };
